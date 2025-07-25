1a563e72035d69b0135e98a21bdaa06b0b7bea17
刘敏清 ch <2985861993@qq.com>
2025-07-26 06:06:47 +0800

    重写Swapchian的重建

--- Engine/src/core/engine.cpp
+++ Engine/src/core/engine.cpp
@@ -20,18 +20,101 @@ namespace MQEngine
         m_wnd = m_rt.createWindow(800,600,"MQ Engine");
         m_ctx = m_rt.createContext();
         m_ctx->create();
+        m_wnd->enableDepthBuffer(Format::D32_SFLOAT_S8_UINT);
         m_wnd->bind(m_ctx);
+        m_ctx->maxFrameInFlight(5);
 
+        m_autoViewport = AutoViewport({800,600},{800,600});
+        m_autoViewport.ctx(m_ctx);
+        m_wnd->getCallBack()->addResizeCallback([this](Window* w,int width,int height)
+        {
+            m_autoViewport.resize(width,height);
+        });
+
+        //setting up pass
         m_imguiPass = m_ctx->createPass();
         m_imguiPass->enableClear(ClearType::color | ClearType::depthStencil,
-            Vec4(1,1,1,1));
+            Vec4(0,0,0,1));
         m_imguiPass->bindTarget(0,m_wnd->getCurrentTarget()->targetImage());
+        m_imguiPass->depthStencil(m_wnd->getCurrentTarget()->depthStencilBuffer());
         m_defaultPassGroup = m_ctx->createPassGroup();
         m_defaultPassGroup->addPass(m_imguiPass);
         m_defaultPassGroup->create();
 
         m_imguiCtx = m_imguiModule.createContext(m_wnd,m_ctx);
         m_imguiCtx->create(m_imguiPass);
+
+        //settting up shader
+        m_vs = m_ctx->createVertexShader();
+        m_vs->addLayout(0,vertexLayout);
+        m_vs->pixelLayout(pixelLayout);
+        m_vs->addUniform(constLayout);
+        m_vs->code(R"(
+ShaderOut main(ShaderIn sIn) {
+    ShaderOut sOut;
+    sOut.color = sIn.color;
+    sOut.position = mul(mvp,sIn.position);
+    sOut.texCoord = sIn.texCoord;
+    sOut.normal = sIn.normal;
+    sOut.srcpos = sIn.position;
+    return sOut;
+}
+)");
+        m_vs->create();
+        m_ps = m_ctx->createPixelShader();
+        m_ps->pixelLayout(pixelLayout);
+        m_ps->addUniform(constLayout);
+        m_ps->code(R"(
+ShaderOut main(ShaderIn sIn) {
+    ShaderOut sOut;
+    float4 lightDir = normalize(lightPos - sIn.srcpos);
+    float diff = max(dot(sIn.normal, lightDir), 0.0);
+    float4 viewPos = float4(40.0,40.0,-40.0,1.0);
+    float4 viewDir = normalize(viewPos - sIn.srcpos);
+    float spec = pow(max(dot(sIn.normal, normalize(lightDir.xyz + viewDir.xyz)), 0.0), 32);
+    sOut.target0 = float4(sIn.color.xyz * (diff + spec * 0.25 + 0.1) , 1.0);
+    return sOut;
+}
+)");
+        m_ps->create();
+
+        //RasterizationState* state = m_ctx->create;
+        //setting up pipeline
+        m_pipeline = m_ctx->createTraditionPipeline();
+        m_pipeline->vertexLayout(vertexLayout);
+        m_pipeline->pixelLayout(pixelLayout);
+        m_pipeline->addResources(m_vs);
+        m_pipeline->addResources(m_ps);
+        m_pipeline->bindPass(m_imguiPass);
+        m_pipeline->create();
+
+        //setting up mesh
+        m_mesh = m_ctx->loadMesh(
+            "ball.obj","ball",vertexLayout);
+        m_floor = m_ctx->loadMesh(
+            "ball.obj","floor",vertexLayout);
+        m_uniform = new UniformBuffer(constLayout);
+
+        Mat4 view = Mat4::LookAt(Vec3(40,40,-40), Vec3(0,0,0), Vec3(0,-1,0));
+        Mat4 proj = Mat4::Perspective(45.0f, 800.0f / 600.0f, 0.1f, 100.0);;
+
+        Mat4 modelMatrix = Mat4();
+        Mat4 mvpMatrix =  view * proj * modelMatrix;
+        m_uniform->setValue("mvp", mvpMatrix);
+        m_lightPos = Vec4(40,0,0,1);
+        m_uniform->setValue("lightPos", m_lightPos);
+        m_constBuffer = m_ctx->createConstBuffer();
+        m_constBuffer->buffer(m_uniform);
+        m_constBuffer->layout(constLayout);
+        m_constBuffer->create();
+        m_constBuffer->mapData();
+
+        m_resource = m_ctx->createPassResource();
+        m_resource->addConstBuffer(m_constBuffer);
+        m_resource->bind(m_wnd);
+        m_resource->create();
+
+        //setting up stencil buffer
 
         //setting up ticker graph
         auto& tickerGraph = m_ctx->submitTickers();
@@ -43,8 +126,16 @@ namespace MQEngine
                 auto cmdBuf = m_ctx->getCmdBuf(m_wnd, 0);
                 cmdBuf->reset();
                 cmdBuf->begin();
+
                 m_defaultPassGroup->beginSubmit(cmdBuf);
                 m_imguiPass->beginSubmit(cmdBuf);
+                m_pipeline->bind(cmdBuf);
+                m_resource->bind(cmdBuf,m_pipeline);
+                m_autoViewport.submit(cmdBuf);
+                m_mesh->bind(cmdBuf);
+                m_mesh->draw(cmdBuf);
+                m_floor->bind(cmdBuf);
+                m_floor->draw(cmdBuf);
                 m_imguiCtx->submit(cmdBuf);
                 m_imguiPass->endSubmit();
                 m_defaultPassGroup->endSubmit(cmdBuf);
@@ -74,6 +165,15 @@ namespace MQEngine
     {
         while (m_wnd->isRunning())
         {
+            static auto lastFrameTime = std::chrono::high_resolution_clock::now();
+            auto currentTime = std::chrono::high_resolution_clock::now();
+            float deltaTime = std::chrono::duration_cast<std::chrono::microseconds>(currentTime - lastFrameTime).count() / 1000000.0f;
+            lastFrameTime = currentTime;
+            Mat4 mat;
+            mat.rotateY(deltaTime * 90);
+            m_lightPos = mat * m_lightPos;
+            m_uniform->setValue("lightPos", m_lightPos);
+            m_constBuffer->updataData();
             m_imguiCtx->push([]()
             {
                 ImGui::Begin("MQ Engine");
--- Engine/src/core/engine.h
+++ Engine/src/core/engine.h
@@ -31,6 +31,34 @@ namespace MQEngine {
         FCT::ImguiModule m_imguiModule;
         FCT::RHI::PassGroup* m_defaultPassGroup;
         FCT::RHI::Pass* m_imguiPass;
+
+
+        FCT::StaticMesh<uint32_t>* m_mesh;
+        FCT::StaticMesh<uint32_t>* m_floor;
+        FCT::VertexLayout vertexLayout = {
+            FCT::VertexElement{FCT::VtxType::Color4f,"color"},
+            FCT::VertexElement{FCT::VtxType::Position4f,"position"},
+            FCT::VertexElement{FCT::VtxType::TexCoord2f,"texCoord"},
+            FCT::VertexElement{FCT::VtxType::Normal3f,"normal"}
+        };
+        FCT::PixelLayout pixelLayout = {
+            FCT::VertexElement{FCT::VtxType::Custom,"srcpos",FCT::Format::R32G32B32A32_SFLOAT},
+            vertexLayout
+        };
+        FCT::ConstLayout constLayout = {
+            "base",
+            FCT::ConstElement{FCT::ConstType::MVPMatrix,"mvp"},
+            FCT::ConstElement{FCT::ConstType::Vec4,"lightPos"},
+        };
+        FCT::VertexShader* m_vs;
+        FCT::PixelShader* m_ps;
+        FCT::RHI::RasterizationPipeline* m_pipeline;
+        FCT::UniformBuffer* m_uniform;
+        FCT::RHI::ConstBuffer* m_constBuffer;
+        FCT::PassResource* m_resource;
+        FCT::Vec4 m_lightPos;
+        FCT::AutoViewport m_autoViewport;
+        FCT::MutilBufferImage* m_mutilBufferImage;
     };
     /**
      * @return engine version
