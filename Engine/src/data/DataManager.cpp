ced33462754e84fc68514fb1db4e51374032ecde
刘敏清 ch <2985861993@qq.com>
2025-08-21 14:38:05 +0800

    添加清理编译，addPass移动到编译的时候了

---
+++ Engine/src/data/DataManager.cpp
@@ -0,0 +1,8 @@
+﻿//
+// Created by Administrator on 2025/8/20.
+//
+
+#include "DataManager.h"
+namespace MQEngine {
+
+}
---
+++ Engine/src/data/DataManager.h
@@ -0,0 +1,19 @@
+﻿//
+// Created by Administrator on 2025/8/20.
+//
+
+#ifndef DATAMANAGER_H
+#define DATAMANAGER_H
+#include "../EnginePCH.h"
+
+namespace MQEngine {
+    class ENGINE_API DataManager {
+    public:
+
+    private:
+
+    };
+}
+
+
+#endif //DATAMANAGER_H
