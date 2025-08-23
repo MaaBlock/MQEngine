14aaa4a833cea07a416a6d43641a3ba54dae950c
刘敏清 ch <2985861993@qq.com>
2025-08-23 14:36:06 +0800

    为RenderGraphView添加对窗口target/depthStencil的支持

---
+++ Editor/src/RenderGraphView/NodeTutorials.cpp
@@ -0,0 +1,45 @@
+﻿#include "PassGenerator.h"
+#include "../Thirdparty/thirdparty.h"
+#include <fstream>
+#include <functional>
+#include <iostream>
+#define NOMINMAX
+#include <Windows.h>
+
+#define TEXT(str) (const char*)u8##str
+
+using namespace FCT;
+namespace MQEngine {
+    static int HashPin(int nodeId, const std::string& pinType, int index)
+    {
+        std::hash<std::string> hasher;
+        std::string pinString = std::to_string(nodeId) + "_" + pinType + "_" + std::to_string(index);
+        int pinId = static_cast<uint32_t>(hasher(pinString));
+        return pinId;
+    }
+
+    int PassNode::targetPinId(int index) const
+    {
+        return HashPin(id, "target", index);
+    }
+
+    int PassNode::depthStencilPinId()
+    {
+        return HashPin(id, "depth", 0);
+    }
+
+    int ImageNode::texturePinId()
+    {
+        return HashPin(id, "texture", 0);
+    }
+
+    int ImageNode::targetPinId()
+    {
+        return HashPin(id, "target", 0);
+    }
+
+    int ImageNode::depthStencilPinId()
+    {
+        return HashPin(id, "depth", 0);
+    }
+}
