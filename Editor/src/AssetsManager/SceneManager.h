b177c2057727c885acadab8035a039ab84ef6bf8
刘敏清 ch <2985861993@qq.com>
2025-08-24 19:44:13 +0800

    场景管理

---
+++ Editor/src/AssetsManager/SceneManager.h
@@ -0,0 +1,19 @@
+﻿//
+// Created by Administrator on 2025/8/24.
+//
+
+#ifndef SCENEMANAGER_H
+#define SCENEMANAGER_H
+
+namespace MQEngine {
+
+    class SceneManager {
+    public:
+        SceneManager();
+    private:
+
+    };
+
+} // MQEngine
+
+#endif //SCENEMANAGER_H
---
+++ Engine/src/system/TechSystem.cpp
@@ -0,0 +1,9 @@
+﻿//
+// Created by Administrator on 2025/8/24.
+//
+
+#include "TechSystem.h"
+
+namespace MQEngine {
+
+} // MQEngine
