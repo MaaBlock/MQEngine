d31da4a65e906ef7bdfee4045ec1a134db715bec
刘敏清 ch <2985861993@qq.com>
2025-09-01 17:24:09 +0800

    光照添加enable参数，为接下来添加光照系统做准备

--- Res/scripts/js/test_rotation.js
+++ Res/scripts/js/test_rotation.js
@@ -1,31 +1,9 @@
 function testRotation() {
-    console.log("Testing rotation component modification...");
-    
-    if (entityInfo.addComponent("RotationComponent")) {
-        console.log("RotationComponent added successfully");
-        
+    if (entityInfo.hasComponent("RotationComponent")) {
         let rotationComponent = entityInfo.getComponent("RotationComponent");
-        console.log("Current rotation component:", rotationComponent);
-        
-        let newRotation = {
-            rotation: {
-                x: 45.0,
-                y: 90.0,
-                z: 180.0
-            }
-        };
-        
-        if (entityInfo.setComponent("RotationComponent", newRotation)) {
-            console.log("Set rotation to (45, 90, 180) degrees");
-            
-            let updatedComponent = entityInfo.getComponent("RotationComponent");
-            console.log("Updated rotation component:", updatedComponent);
-            
-            console.log("Rotation test completed successfully!");
-        } else {
-            console.log("Failed to set rotation component");
-        }
+        rotationComponent.rotation.y += engine.logicDealt * 10;
+        entityInfo.setComponent("RotationComponent", rotationComponent);
     } else {
-        console.log("Failed to add RotationComponent");
+        console.log("实体没有 RotationComponent 组件.");
     }
 }
